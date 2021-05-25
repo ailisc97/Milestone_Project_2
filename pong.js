7e37e0ca365e39a35697b11240f2aa109a11a3cf
ailisc97 <ailiscurran97@gmail.com>
2021-05-25 14:28:41 +0000

    fixing the positioning of the ball

--- pong.js
+++ pong.js
@@ -1,25 +1,33 @@
+//Selecting the Canvas
 const canvas = document.getElementsById("pong");
 const context = canvas.getContext("2d"); //Methods and Properties
 
 //Drawing the Rectangle
     function drawRect(x,y,w,h,color){
-    context.fillStyle="black";
-    context.fillRect(100,200,50,75); //x-axis, y-axis, height and width
+    context.fillStyle=color;
+    context.fillRect(x,y,w,h); //x-axis, y-axis, width and height
 }
-//Drawing the Ball
+
+drawRect(0,0,cvs.width,csv.height,"BLACK");
+
+//Drawing the Circle
     function drawCircle(x,y,r,color){
-    context.fillStyle="red";
+    context.fillStyle=color;
     context.beginPath();
     context.arc (300,350,100,0,Math.PI*2,false );//x position, y position, radius, Start Angle,End angle(360), direction (clockwise)
     context.closePath();
 }
 
+drawCircle(100,100,50, "WHITE");
+
 //Drawing Text
-function drawText(text,x,y,color){
-    context.fillStyle="red";
-    context.font="75px fantasy";
+function drawText(){
+    context.fillStyle=color;
+    context.font="45px fantasy";
     context.fillText(text,x,y);
 }
+
+drawText("something",300,200,"WHITE");
 
 //Moving the Rectangle
 let rectX=0;
