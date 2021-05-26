8b01dffa19fe0ba032f077cd256dc77bcebea8c3
ailisc97 <ailiscurran97@gmail.com>
2021-05-26 14:21:09 +0000

    Added the class pong detail

---
+++ src/pong.js
@@ -0,0 +1,129 @@
+class Vec
+{
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    get len()
+    {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    set len(value) {
+        const f = value / this.len;
+        this.x *= f;
+        this.y *= f;
+    }
+}
+
+class Rect
+{
+    constructor(x = 0, y = 0)
+    {
+        this.pos = new Vec(0, 0);
+        this.size = new Vec(x, y);
+    }
+    get left()
+    {
+        return this.pos.x - this.size.x / 2;
+    }
+    get right()
+    {
+        return this.pos.x + this.size.x / 2;
+    }
+    get top()
+    {
+        return this.pos.y - this.size.y / 2;
+    }
+    get bottom()
+    {
+        return this.pos.y + this.size.y / 2;
+    }
+}
+
+class Ball extends Rect
+{
+    constructor()
+    {
+        super(10, 10);
+        this.vel = new Vec;
+    }
+}
+
+class Player extends Rect
+{
+    constructor()
+    {
+        super(20, 100);
+        this.vel = new Vec;
+        this.score = 0;
+
+        this._lastPos = new Vec;
+    }
+    update(dt)
+    {
+        this.vel.y = (this.pos.y - this._lastPos.y) / dt;
+        this._lastPos.y = this.pos.y;
+    }
+}
+
+class Pong
+{
+    constructor(canvas)
+    {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        this.initialSpeed = 250;
+
+        this.ball = new Ball;
+
+        this.players = [
+            new Player,
+            new Player,
+        ];
+
+        this.players[0].pos.x = 40;
+        this.players[1].pos.x = this._canvas.width - 40;
+        this.players.forEach(p => p.pos.y = this._canvas.height / 2);
+
+        let lastTime = null;
+        this._frameCallback = (millis) => {
+            if (lastTime !== null) {
+                const diff = millis - lastTime;
+                this.update(diff / 1000);
+            }
+            lastTime = millis;
+            requestAnimationFrame(this._frameCallback);
+        };
+
+        this.CHAR_PIXEL = 10;
+        this.CHARS = [
+            '111101101101111',
+            '010010010010010',
+            '111001111100111',
+            '111001111001111',
+            '101101111001001',
+            '111100111001111',
+            '111100111101111',
+            '111001001001001',
+            '111101111101111',
+            '111101111001111',
+        ].map(str => {
+            const canvas = document.createElement('canvas');
+            const s = this.CHAR_PIXEL;
+            canvas.height = s * 5;
+            canvas.width = s * 3;
+            const context = canvas.getContext('2d');
+            context.fillStyle = '#fff';
+            str.split('').forEach((fill, i) => {
+                if (fill === '1') {
+                    context.fillRect((i % 3) * s, (i / 3 | 0) * s, s, s);
+                }
+            });
+            return canvas;
+        });
+
+        this.reset();
+    }
+    
