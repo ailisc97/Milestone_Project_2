799093ddc1dc1c775fadaed9c0d3b8cc4ffa32e0
ailisc97 <ailiscurran97@gmail.com>
2021-05-27 12:24:02 +0000

    added EmailJS to the result.html

--- src/sendEmail.js
+++ src/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function sendMail(resultForm) {
+    emailjs.send("service_dmqixxl", "template_kc5b6ij", {
+        "from_name": resultForm.name.value,
+        "from_email": resultForm.emailaddress.value,
+        "Score": resultForm.score.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
